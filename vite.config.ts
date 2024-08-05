5dc40202b34f60123d543586bd89be6295e96cce
Lawrence <lawrence891106@gmail.com>
2024-08-05 22:05:05 +0800

    testing new base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lawrence565.github.io/e-commerce-demo/",
+  base: "/e-commerce-demo",
   css: {
     postcss: "./postcss.config.js",
   },
