6ec63f38deb62b014bfff5917d1730c3337cb703
Lawrence <lawrence891106@gmail.com>
2024-08-07 20:13:31 +0800

    Refine cart experience and update coupon function

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL,
+  // base: "/e-commerce-demo",
   css: {
     postcss: "./postcss.config.js",
   },
