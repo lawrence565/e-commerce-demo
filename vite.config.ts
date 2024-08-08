26aeb32bd22c4902cfd32db1ebcf2f00bd8dbb3c
Lawrence <lawrence891106@gmail.com>
2024-08-08 23:38:52 +0800

    Merging from test

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: "/e-commerce-demo",
-=======
   base: process.env.VITE_BASE_URL,
->>>>>>> test
   css: {
     postcss: "./postcss.config.js",
   },
