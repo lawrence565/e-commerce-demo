a472a5c031183bd0ec7113e064740484705bb548
Lawrence <lawrence891106@gmail.com>
2025-08-01 12:09:56 +0800

    Update GA4 script to use defer attribute

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_URL,
+  server: {
+    allowedHosts: [
+      "3dc3-2407-4d00-1c00-8841-3968-6998-2fc0-9572.ngrok-free.app",
+    ],
+    strictPort: true,
+    cors: true,
+  },
   css: {
     postcss: "./postcss.config.js",
   },
